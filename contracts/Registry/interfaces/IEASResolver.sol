e6c6fb9df1289b293662bc2a3f4d970e778276c3
0xneves <asguilift@hotmail.com>
2024-08-28 21:04:28 -0300

    fix: removed unused code

--- contracts/Registry/interfaces/IEASResolver.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
-
-/// @notice The interface of the {Resolver} contract.
-interface IEASResolver {
-  /// @notice Returns the owner of the project.
-  /// @param key The key of the project.
-  function projectOwner(bytes32 key) external view returns (address);
-}

ebbc1ec8eea4aceb2d5a8fe53b433a0e809143d9
0xneves <asguilift@hotmail.com>
2024-08-30 14:24:43 -0300

    refactor: more concise grant program choices

--- scripts/registerGrants.ts
+++ scripts/registerGrants.ts
@@ -5,24 +5,31 @@ import abi from "../artifacts/contracts/registry/GrantRegistry.sol/GrantRegistry
 const grants = [
   {
     grantId: "0x635c2d0642c81e3191e6eff8623ba601b7e22e832d7791712b6bc28d052ff2b5",
-    programId: 260,
+    programId: 100,
     grantee: "0x67eE5d3A5374849aDB4D80713a5765F164375F03",
     chain: 42161,
     status: 0,
   },
   {
-    grantId: "0x8f11bdca983f72b840c4bbc4c2b96ebf4292872669619eac8bb24d88dedcef99",
-    programId: 260,
+    grantId: "0x1dac0e2beeba6f3eec76117545f39f28e8ecc3d2c22731b6072b2d87e82fa35d",
+    programId: 100,
     grantee: "0x67eE5d3A5374849aDB4D80713a5765F164375F03",
     chain: 42161,
     status: 1,
   },
   {
     grantId: "0xcdaa67fc005df1417c7f9a54eee0d62ec3b44d157eba707b3a0169d4cbca9bf3",
-    programId: 260,
+    programId: 100,
     grantee: "0x67eE5d3A5374849aDB4D80713a5765F164375F03",
     chain: 42161,
     status: 2,
+  },
+  {
+    grantId: "0x8f11bdca983f72b840c4bbc4c2b96ebf4292872669619eac8bb24d88dedcef99",
+    programId: 99,
+    grantee: "0x67eE5d3A5374849aDB4D80713a5765F164375F03",
+    chain: 42161,
+    status: 1,
   },
 ];
 

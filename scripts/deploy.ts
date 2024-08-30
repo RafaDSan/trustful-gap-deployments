d3b94c6e5ff752296e4c5180a8fd083c6bcf28a8
0xneves <asguilift@hotmail.com>
2024-08-30 14:17:13 -0300

    refactor: logging addresses after each deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -19,22 +19,25 @@ async function main() {
   console.log("Deploying contracts...\n");
 
   const grantRegistry = await GrantRegistry.deploy();
+  console.log("GRANT_REGISTRY deployed to:", grantRegistry.address);
+
   const badgeRegistry = await BadgeRegistry.deploy();
+  console.log("BADGE_REGISTRY deployed to:", badgeRegistry.address);
+
   const trustfulScorer = await TrustfulScorer.deploy();
+  console.log("TRUSTFUL_SCORER deployed to:", trustfulScorer.address);
+
   const resolverEAS = await ResolverEAS.deploy(
     process.env.ARB_ONE_EAS as string,
     grantRegistry.address,
     badgeRegistry.address,
   );
+  console.log("RESOLVER_EAS deployed to:", resolverEAS.address);
+
   const resolverTrustful = await ResolverTrustful.deploy(
     trustfulScorer.address,
     resolverEAS.address,
   );
-
-  console.log("GRANT_REGISTRY deployed to:", grantRegistry.address);
-  console.log("BADGE_REGISTRY deployed to:", badgeRegistry.address);
-  console.log("TRUSTFUL_SCORER deployed to:", trustfulScorer.address);
-  console.log("RESOLVER_EAS deployed to:", resolverEAS.address);
   console.log("RESOLVER_TRUSTFUL deployed to:", resolverTrustful.address);
 }
 
